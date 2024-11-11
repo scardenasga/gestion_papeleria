2d84b9ffd39e3c594121ab1c10176d6c053ca1a5
Reywood06 <97995461+Reywood06@users.noreply.github.com>
2024-11-11 14:17:39 -0500

    Update DetalleContableDAO.java

--- src/main/java/co/edu/unbosque/model/dao/DetalleContableDAO.java
+++ src/main/java/co/edu/unbosque/model/dao/DetalleContableDAO.java
@@ -1,12 +1,49 @@
-package co.edu.unbosque.model.dao;
+package co.edu.unbosque.model.entity;
 
-import co.edu.unbosque.model.entity.DetalleContable;
+import java.io.Serializable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class DetalleContableDAO extends GenericDAO<Long, DetalleContable> {
- 
+import java.math.BigDecimal;
 
-    public DetalleContableDAO(){
-        super(DetalleContable.class); 
-    }
-    
+
+/**
+ * The persistent class for the detalle_contable database table.
+ * 
+ */
+@Entity
+@Table(name="detalle_contable")
+@NamedQuery(name="DetalleContable.findAll", query="SELECT d FROM DetalleContable d")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DetalleContable implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="id_detalle")
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long idDetalle;
+
+	@Column(name = "debe")
+	private BigDecimal debe;
+
+	@Column(name = "haber")
+	private BigDecimal haber;
+
+	//bi-directional many-to-one association to AsientoContable
+	@ManyToOne
+	@JoinColumn(name="id_asiento")
+	private AsientoContable asientoContable;
+
+	//bi-directional many-to-one association to CuentaContable
+	@ManyToOne
+	@JoinColumn(name="id_cuenta")
+	private CuentaContable cuentaContable;
+
 }
